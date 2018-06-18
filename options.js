2f559d7fd9b9ca478d71cbd147e8d51921078a7e
brandinchiu <brandin@spoonity.com>
2018-06-18 15:54:49 -0400

    added example extension

---
+++ options.js
@@ -0,0 +1,20 @@
+let page = document.getElementById('buttonDiv');
+
+const kbuttonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+
+function constructOptions(kbuttonColors) {
+    for(let item of kbuttonColors) {
+        let button = document.createElement('button');
+
+        button.style.backgroundColor = item;
+        button.addEventListener('click', function() {
+            chrome.storage.sync.set({color: item}, function() {
+                console.log('the color is ' + item);
+            });
+        });
+
+        page.appendChild(button);
+    }
+}
+
+constructOptions(kbuttonColors);
